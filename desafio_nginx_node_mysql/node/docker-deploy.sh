83a6ee8b33c1bbb7fee5146cc930f257f0128fc5
fernando <generoso.fernando@gmail.com>
2022-12-09 11:34:17 -0300

    refactor

--- desafio_nginx_node_mysql/node/docker-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# make wait-for-it executable
-#chmod +x wait-for-it.sh
-
-# call wait-for-it with passed in args and then start node if it succeeds
-bash wait-for-it.sh -h db -p 3306 -t 300 -s -- node index.js
